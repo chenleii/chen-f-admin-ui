574be305f4d6438459e2c69b3da2bb95111d36de
chenlei <1@1.com>
2020-04-19 12:05:08 +0800

    view

--- src/app/routes/sys/menu/view/view.component.ts
+++ src/app/routes/sys/menu/view/view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NzModalRef, NzMessageService } from 'ng-zorro-antd';
 import { _HttpClient } from '@delon/theme';
+import { zip } from 'rxjs';
+import { CacheService } from '@delon/cache';
 
 @Component({
   selector: 'app-sys-menu-view',
@@ -8,16 +10,28 @@ import { _HttpClient } from '@delon/theme';
 })
 export class SysMenuViewComponent implements OnInit {
   record: any = {};
-  i: any;
+  sysMenu: any;
 
   constructor(
     private modal: NzModalRef,
     public msgSrv: NzMessageService,
-    public http: _HttpClient
+    public http: _HttpClient,
+    private cacheService: CacheService,
   ) { }
 
   ngOnInit(): void {
-    this.http.get(`/user/${this.record.id}`).subscribe(res => this.i = res);
+    zip(
+      this.http.get(`/chen/admin/sys/menu/${this.record.id}`,),
+      this.cacheService.get('/chen/common/sys/dictionary/item/alain/tag/SYS_MENU.TYPE',
+        { mode: 'promise', type: 's', expire: 86400 }),
+      this.cacheService.get('/chen/common/sys/dictionary/item/alain/tag/STATUS',
+        { mode: 'promise', type: 's', expire: 86400 }),
+    ).subscribe(([sysMenu,typeTag, statusTag]: any[]) => {
+
+      sysMenu.type = typeTag[sysMenu.type];
+      sysMenu.status = statusTag[sysMenu.status];
+      this.sysMenu = sysMenu;
+    });
   }
 
   close() {
--- src/app/routes/sys/parameter/view/view.component.ts
+++ src/app/routes/sys/parameter/view/view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NzModalRef, NzMessageService } from 'ng-zorro-antd';
 import { _HttpClient } from '@delon/theme';
+import { zip } from 'rxjs';
+import { CacheService } from '@delon/cache';
 
 @Component({
   selector: 'app-sys-parameter-view',
@@ -8,16 +10,32 @@ import { _HttpClient } from '@delon/theme';
 })
 export class SysParameterViewComponent implements OnInit {
   record: any = {};
-  i: any;
+  sysParameter: any;
 
   constructor(
     private modal: NzModalRef,
     public msgSrv: NzMessageService,
-    public http: _HttpClient
-  ) { }
+    public http: _HttpClient,
+    public cacheService: CacheService,
+  ) {
+  }
 
   ngOnInit(): void {
-    this.http.get(`/user/${this.record.id}`).subscribe(res => this.i = res);
+    zip(
+      this.http.get(`/chen/admin/sys/parameter/${this.record.id}`),
+      this.cacheService.get('/chen/common/sys/dictionary/item/alain/tag/VALUE_TYPE',
+        { mode: 'promise', type: 's', expire: 86400 }),
+      this.cacheService.get('/chen/common/sys/dictionary/item/alain/tag/SYS_PARAMETER.TYPE',
+        { mode: 'promise', type: 's', expire: 86400 }),
+      this.cacheService.get('/chen/common/sys/dictionary/item/alain/tag/STATUS',
+        { mode: 'promise', type: 's', expire: 86400 }),
+    ).subscribe(([sysParameter, valueTypeTag, typeTag, statusTag]: any[]) => {
+
+      sysParameter.valueType = valueTypeTag[sysParameter.valueType];
+      sysParameter.type = typeTag[sysParameter.type];
+      sysParameter.status = statusTag[sysParameter.status];
+      this.sysParameter = sysParameter;
+    });
   }
 
   close() {
